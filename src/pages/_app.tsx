2d228170c7144c59603ec64e757a370941333740
bonabrian <bonabrian@investree.id>
2022-06-28 14:41:45 +0700

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import '@/styles/nprogress.css'
+
 import type { EmotionCache } from '@emotion/cache'
 import { CacheProvider } from '@emotion/react'
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import nProgress from 'nprogress'
 
 import Layout from '@/common/components/layout'
 import { createEmotionCache } from '@/common/utils'
@@ -11,6 +15,10 @@ const clientSideEmotionCache = createEmotionCache()
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
 }
+
+Router.events.on('routeChangeStart', nProgress.start)
+Router.events.on('routeChangeError', nProgress.done)
+Router.events.on('routeChangeComplete', nProgress.done)
 
 const MyApp = ({
   Component,
