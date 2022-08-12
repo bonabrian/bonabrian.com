469134a7b0ec2271c87282928cc7800ece5a09be
bonabrian <bonabriansiagian@gmail.com>
2022-08-13 01:53:34 +0700

    feat: add loading spinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import { CgSpinner } from 'react-icons/cg'
+
+const LoadingSpinner = () => {
+  return (
+    <CgSpinner className='animate-spin h-4 w-4 text-primary-600 dark:text-primary-400' />
+  )
+}
+
+export default LoadingSpinner
