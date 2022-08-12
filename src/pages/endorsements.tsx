6e7e04960b4213061aae376a35b583330f32757c
bonabrian <bonabriansiagian@gmail.com>
2022-08-13 02:43:42 +0700

    feat: init endorsements page

--- src/pages/endorsements.tsx
+++ src/pages/endorsements.tsx
@@ -1,12 +1,37 @@
-const Endorsements = () => (
-  <div className='endorsements'>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam
-      dicta quia ipsam quae reiciendis quaerat illo consequatur repellendus
-      tenetur deserunt ipsa, natus, facere maxime ratione error adipisci magnam
-      laudantium!
-    </p>
-  </div>
-)
+import type { InferGetStaticPropsType } from 'next'
+
+import { PageMeta } from '@/components/Meta'
+import PageTitle from '@/components/PageTitle'
+import Skills from '@/components/Skills'
+import { getSkillsByCategory } from '@/lib/db'
+
+export const getStaticProps = async () => {
+  const skillsByCategory = await getSkillsByCategory()
+
+  return {
+    props: {
+      fallbackData: skillsByCategory,
+    },
+    revalidate: 60,
+  }
+}
+
+const Endorsements = ({
+  fallbackData,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <>
+      <PageMeta title='Endorsements' description={"bonabrian's Endorsements"} />
+      <div className='flex flex-col items-start justify-center max-w-2xl pt-6 pb-8 space-y-2 md:space-y-5'>
+        <PageTitle>Endorsements</PageTitle>
+        <p className='text-lg leading-7 text-gray-500 dark:text-gray-400 xl:text-xl'>
+          Since you&apos;re here, I invite you to give me endorsement(s) based
+          on the experience you had with me in tech.
+        </p>
+      </div>
+      <Skills fallbackData={fallbackData} />
+    </>
+  )
+}
 
 export default Endorsements
