c02de5643c64f2837f95c35c369bb440ff651da4
bonabrian <bonabriansiagian@gmail.com>
2022-09-11 19:24:17 +0700

    refactor: enhance divider component

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,5 +1,5 @@
 const Divider = () => {
-  return <hr className='my-5 mx-0 h-px border-0 bg-gray-500' />
+  return <hr className='my-8 mx-0 h-px border-0 bg-gray-500' />
 }
 
 export default Divider
