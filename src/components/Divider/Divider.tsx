2b31153dcf49f6541dfd5f02bf9f8275bcb2c34d
bonabrian <bonabriansiagian@gmail.com>
2022-09-16 19:42:46 +0700

    refactor: update divider

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,5 +1,7 @@
 const Divider = () => {
-  return <hr className='my-8 mx-0 h-px border-0 bg-gray-500' />
+  return (
+    <hr className='w-full h-10 my-2 text-2xl border-none text-center animate-pulse before:content-["∿∿∿"] before:text-gray-800 before:dark:text-gray-500' />
+  )
 }
 
 export default Divider
