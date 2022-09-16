64499e22517d79f8d3e646fa7fb45865af963d99
bonabrian <bonabriansiagian@gmail.com>
2022-09-16 09:03:53 +0700

    refactor: enhance tag component

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -5,7 +5,7 @@ import Link from '../Link'
 const Tag = ({ text }: { text: string }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`} className='no-underline'>
-      <span className='inline-flex text-sm lowercase bg-purple-500 dark:bg-purple-600 text-white px-3 py-1 rounded-lg'>
+      <span className='inline-flex text-sm lowercase bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-300 px-2 py-0.5 rounded-full'>
         {text}
       </span>
     </Link>
