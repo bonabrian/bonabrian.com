0ec46959be8720b5ef61a3bf77757355088f81b3
bonabrian <bonabriansiagian@gmail.com>
2022-10-01 05:00:18 +0700

    fix: invalid dialog position

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -51,7 +51,7 @@ const SignIn = ({
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as='div'
-          className='fixed inset-0 z-10 overflow-y-auto'
+          className='fixed inset-0 z-20 overflow-y-auto'
           onClose={() => setIsOpen(false)}
         >
           <div className='min-h-screen px-4 text-center'>
