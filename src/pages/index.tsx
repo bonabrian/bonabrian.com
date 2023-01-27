bd1d7cc5fc142dcc6ca153fe9ef27ef0c4fdd6f8
bonabrian <bonabriansiagian@gmail.com>
2023-01-27 22:39:42 +0700

    feat: move copywriting to Introduction component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { AnimatePresence, motion } from 'framer-motion'
 import type { InferGetStaticPropsType } from 'next'
 
-import { DiscoverMore } from '@/components/DiscoverMore'
 import Divider from '@/components/Divider'
+import Introduction from '@/components/Introduction'
 import Link from '@/components/Link'
 import PageSeo from '@/components/PageSeo'
 import { PostCard } from '@/components/PostCard'
@@ -41,24 +41,10 @@ const Home = ({
           'development',
         ]}
       />
-      <div className="flex flex-col justify-evenly items-center w-full h-content sm:h-content-sm">
-        <div className="flex flex-col">
-          <h1 className="my-5 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, I&apos;m{' '}
-            <span className="text-primary-500 dark:text-primary-500">
-              Bona Brian Siagian
-            </span>{' '}
-            <span className="block">Full Stack Developer</span>
-          </h1>
-          <div className="text-base sm:text-xl">
-            <p>
-              I like to build interactive things with code. I also talk and
-              write about those things.
-            </p>
-          </div>
-        </div>
-        <DiscoverMore label="know more about me" href="/about" />
-      </div>
+
+      <Introduction />
+
+      <Divider />
 
       <div className="flex justify-between items-center my-4 sm:my-8">
         <h2 className="text-xl font-extrabold leading-5 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-7 md:text-3xl md:leading-9">
