e32f80336263beaaba61f619a78e099d67f0c803
bonabrian <bonabriansiagian@gmail.com>
2023-01-27 20:50:51 +0700

    style: remove unused tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,6 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '9/16': '56.25%',
-        0.75: '0.1875rem',
-        'content-sm': 'calc(100vh - 4.5rem)',
         content: 'calc(100vh - 4rem)',
       },
       lineHeight: {
@@ -25,35 +22,23 @@ module.exports = {
         14: '3.5rem',
       },
       letterSpacing: {
-        tightest: '-.075rem',
-      },
-      fontSize: {
-        '8.5xl': '7rem',
+        tightest: '-0.075rem',
       },
       fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      },
-      gradientColorStops: {
-        // https://coolors.co/2d00f7-6a00f4-8900f2-a100f2-b100e8-bc00dd-d100d1-db00b6-e500a4-f20089
-        'gradient-1-start': '#F20089',
-        'gradient-1-end': '#D100D1',
-        'gradient-2-start': '#D100D1',
-        'gradient-2-end': '#A100F2',
-        'gradient-3-start': '#A100F2',
-        'gradient-3-end': '#2D00F7',
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         // Generated from https://colors.eva.design/
         primary: {
-          100: '#EAE0FF',
-          200: '#D3C1FF',
-          300: '#BBA2FF',
-          400: '#A88BFF',
-          500: '#8865FF',
-          600: '#6749DB',
-          700: '#4A32B7',
-          800: '#322093',
-          900: '#21137A',
+          100: '#F2E9FF',
+          200: '#E3D3FF',
+          300: '#D4BDFF',
+          400: '#C6ACFF',
+          500: '#B191FF',
+          600: '#8769DB',
+          700: '#6249B7',
+          800: '#422E93',
+          900: '#2C1B7A',
         },
         success: {
           100: '#F4FCCF',
@@ -66,28 +51,6 @@ module.exports = {
           800: '#497907',
           900: '#396404',
         },
-        info: {
-          100: '#D5FEFB',
-          200: '#ACFEFE',
-          300: '#83F3FC',
-          400: '#63E4FA',
-          500: '#31CCF7',
-          600: '#23A1D4',
-          700: '#187AB1',
-          800: '#0F578F',
-          900: '#093E76',
-        },
-        warning: {
-          100: '#FFF7D7',
-          200: '#FFEDAF',
-          300: '#FFE087',
-          400: '#FFD469',
-          500: '#FFC038',
-          600: '#DB9D28',
-          700: '#B77C1C',
-          800: '#935E11',
-          900: '#7A480A',
-        },
         danger: {
           100: '#FFE9D6',
           200: '#FFCDAD',
@@ -99,16 +62,12 @@ module.exports = {
           800: '#930F18',
           900: '#7A0919',
         },
-        green: colors.emerald,
-        yellow: colors.amber,
-        purple: colors.violet,
         gray: colors.neutral,
-        spotify: '#1DB954',
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.700'),
+            color: theme('colors.black'),
             a: {
               color: theme('colors.primary.500'),
               '&:hover': {
@@ -119,62 +78,25 @@ module.exports = {
             h1: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
+              color: theme('colors.black'),
             },
             h2: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
+              color: theme('colors.black'),
             },
             h3: {
               fontWeight: '600',
-              color: theme('colors.gray.900'),
+              color: theme('colors.black'),
             },
             'h4,h5,h6': {
-              color: theme('colors.gray.900'),
-            },
-            pre: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            code: {
-              color: theme('colors.green.500'),
-              backgroundColor: theme('colors.gray.100'),
-              paddingLeft: '4px',
-              paddingRight: '4px',
-              paddingTop: '2px',
-              paddingBottom: '2px',
-              borderRadius: '0.25rem',
-            },
-            'code:before': {
-              content: 'none',
-            },
-            'code:after': {
-              content: 'none',
-            },
-            hr: { borderColor: theme('colors.gray.200') },
-            'ol li:before': {
-              fontWeight: '600',
-              color: theme('colors.gray.500'),
-            },
-            'ul li:before': {
-              backgroundColor: theme('colors.gray.500'),
-            },
-            'ul li > :last-child': {
-              margin: 0,
-            },
-            'ul li > :first-child': {
-              margin: 0,
-            },
-            strong: { color: theme('colors.gray.600') },
-            blockquote: {
-              color: theme('colors.gray.900'),
-              borderLeftColor: theme('colors.gray.200'),
+              color: theme('colors.black'),
             },
           },
         },
         dark: {
           css: {
-            color: theme('colors.gray.300'),
+            color: theme('colors.slate.100'),
             a: {
               color: theme('colors.primary.500'),
               '&:hover': {
@@ -185,49 +107,19 @@ module.exports = {
             h1: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
+              color: theme('colors.slate.100'),
             },
             h2: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
+              color: theme('colors.slate.100'),
             },
             h3: {
               fontWeight: '600',
-              color: theme('colors.gray.100'),
+              color: theme('colors.slate.100'),
             },
             'h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
-            code: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            hr: { borderColor: theme('colors.gray.700') },
-            'ol li:before': {
-              fontWeight: '600',
-              color: theme('colors.gray.400'),
-            },
-            'ul li:before': {
-              backgroundColor: theme('colors.gray.400'),
-            },
-            'ul li > :last-child': {
-              margin: 0,
-            },
-            'ul li > :first-child': {
-              margin: 0,
-            },
-            strong: { color: theme('colors.gray.100') },
-            thead: {
-              color: theme('colors.gray.100'),
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700'),
-              },
-            },
-            blockquote: {
-              color: theme('colors.gray.100'),
-              borderLeftColor: theme('colors.gray.700'),
+              color: theme('colors.slate.100'),
             },
           },
         },
@@ -241,78 +133,9 @@ module.exports = {
             height: '0.375rem',
           },
         },
-        expand: {
-          '0% , 100%': {
-            height: '0.375rem',
-          },
-          '50%': {
-            height: '0.75rem',
-          },
-        },
-        'gradient-foreground-1': {
-          '0%, 16.667%, 100%': {
-            opacity: 1,
-          },
-          '33.333%, 83.333%': {
-            opacity: 0,
-          },
-        },
-        'gradient-background-1': {
-          '0%, 16.667%, 100%': {
-            opacity: 0,
-          },
-          '25%, 91.667%': {
-            opacity: 1,
-          },
-        },
-        'gradient-foreground-2': {
-          '0%, 100%': {
-            opacity: 0,
-          },
-          '33.333%, 50%': {
-            opacity: 1,
-          },
-          '16.667%, 66.667%': {
-            opacity: 0,
-          },
-        },
-        'gradient-background-2': {
-          '0%, to': {
-            opacity: 1,
-          },
-          '33.333%, 50%': {
-            opacity: 0,
-          },
-          '25%, 58.333%': {
-            opacity: 1,
-          },
-        },
-        'gradient-foreground-3': {
-          '0%, 50%, 100%': {
-            opacity: 0,
-          },
-          '66.667%, 83.333%': {
-            opacity: 1,
-          },
-        },
-        'gradient-background-3': {
-          '0%, 58.333%, 91.667%, 100%': {
-            opacity: 1,
-          },
-          '66.667%, 83.333%': {
-            opacity: 0,
-          },
-        },
       },
       animation: {
         shrink: 'shrink 1.5s infinite',
-        expand: 'expand 1.5s infinite',
-        'gradient-background-1': 'gradient-background-1 8s infinite',
-        'gradient-foreground-1': 'gradient-foreground-1 8s infinite',
-        'gradient-background-2': 'gradient-background-2 8s infinite',
-        'gradient-foreground-2': 'gradient-foreground-2 8s infinite',
-        'gradient-background-3': 'gradient-background-3 8s infinite',
-        'gradient-foreground-3': 'gradient-foreground-3 8s infinite',
       },
     },
   },
