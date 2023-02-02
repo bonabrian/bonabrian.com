1b9f26cc7feb1c1e3e4f6351c2797083b109b7e0
bonabrian <bonabriansiagian@gmail.com>
2023-02-02 23:03:27 +0700

    feat: enhance page header padding

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,5 @@
+import classnames from 'classnames'
+
 interface PageHeaderProps {
   title: string
   description?: string
@@ -5,7 +7,9 @@ interface PageHeaderProps {
 
 const PageHeader = ({ title, description }: PageHeaderProps) => {
   return (
-    <div className="pb-10 sm:pb-15 md:pb-20 flex flex-col">
+    <div
+      className={classnames('flex flex-col', description ? 'pb-4 sm:pb-8' : '')}
+    >
       <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-9 sm:leading-10 md:leading-14 mb-4 sm:mb-6 lg:mb-8 tracking-tight">
         {title}
       </h1>
