4ae36233c186e494760a0df3ff4c9b5b4aee3c7e
bonabrian <bonabriansiagian@gmail.com>
2023-02-03 18:02:32 +0700

    feat: enhance tags page

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -7,8 +7,8 @@ import type {
 import { useMemo, useState } from 'react'
 import { RiSearch2Line } from 'react-icons/ri'
 
+import PageHeader from '@/components/PageHeader'
 import PageSeo from '@/components/PageSeo'
-import PageTitle from '@/components/PageTitle'
 import PostCard from '@/components/PostCard'
 import { filterPosts, getPosts } from '@/lib/contentlayer'
 import { kebabCase } from '@/lib/utils'
@@ -60,26 +60,17 @@ const Tag = ({
 
   const renderSearchComponent = () => {
     return (
-      <>
-        <div className="relative w-full">
-          <input
-            aria-label="Search"
-            type="text"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search"
-            className="block w-full px-10 py-2 text-gray-800 dark:text-gray-200 bg-transparent border border-gray-200 dark:border-gray-600 focus:border-primary-400 dark:focus:border-primary-600 focus:ring-primary-400 dark:focus:ring-primary-600 rounded-md transition ease-in-out duration-200"
-          />
-          <RiSearch2Line className="absolute w-5 h-5 text-gray-400 dark:text-gray-700 fill-current top-3 left-3" />
-        </div>
-        {(filteredPosts?.length || 0) <= 0 ? (
-          <div className="flex items-center justify-center py-3">
-            <h3 className="text-gray-400 dark:text-gray-300">
-              No posts found.
-            </h3>
-          </div>
-        ) : null}
-      </>
+      <div className="relative w-full flex items-center md:max-w-md">
+        <input
+          aria-label="Search"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search posts"
+          className="block w-full px-10 py-2 bg-slate-200 dark:bg-gray-800 border border-transparent focus:border-primary-500 focus:ring-primary-500 rounded-full transition ease-in-out duration-200"
+        />
+        <RiSearch2Line className="absolute w-5 h-5 text-gray-400 dark:text-gray-700 fill-current top-3 left-3" />
+      </div>
     )
   }
 
@@ -98,12 +89,12 @@ const Tag = ({
           'keyword',
         ]}
       />
-      <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-        <PageTitle>{`#${tag}`}</PageTitle>
+      <div className="my-4 space-y-3 md:space-y-5">
+        <PageHeader title={`#${tag}`} />
         {renderSearchComponent()}
       </div>
-      <div className="flex flex-col">
-        <div className="space-y-1 flex flex-col">
+      {filteredPosts.length ? (
+        <div className="flex flex-col gap-8 py-8">
           {filteredPosts.map((post, index) => {
             return (
               <motion.div
@@ -117,7 +108,9 @@ const Tag = ({
             )
           })}
         </div>
-      </div>
+      ) : (
+        <p className="text-center">No posts.</p>
+      )}
     </>
   )
 }
