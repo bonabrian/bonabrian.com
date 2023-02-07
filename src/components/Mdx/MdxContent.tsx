60f56aa9dd52a9382f5b56deacd05e067b3e9b87
bonabrian <bonabriansiagian@gmail.com>
2023-02-08 01:49:24 +0700

    feat: implement Reaction buttons

--- src/components/Mdx/MdxContent.tsx
+++ src/components/Mdx/MdxContent.tsx
@@ -7,6 +7,7 @@ import type { ImageMeta, Post, Snippet } from '@/types'
 
 import Divider from '../Divider'
 import Link from '../Link'
+import Reactions from '../Reactions'
 import ShareArticle from '../ShareArticle'
 import Tag from '../Tag'
 import { Image } from './Image'
@@ -74,7 +75,6 @@ export const MdxContent = ({ content, children }: MdxContentProps) => {
   }, [imageMeta])
 
   const createdAt = formatDate({ timestamp: date })
-  const isSnippet = content.type === 'Snippet'
 
   return (
     <div className="flex flex-col">
@@ -134,16 +134,10 @@ export const MdxContent = ({ content, children }: MdxContentProps) => {
               ))}
             </div>
           )}
-          {!isSnippet && (
-            <div className="flex flex-col justify-center items-center my-4">
-              <p className="text-base my-4">Share this article</p>
-              <ShareArticle
-                slug={slug}
-                title={title}
-                description={description}
-              />
-            </div>
-          )}
+          <div className="flex justify-between items-center my-4">
+            <Reactions slug={slug} />
+            <ShareArticle slug={slug} title={title} description={description} />
+          </div>
           <Divider />
         </div>
       </article>
