4821a48dd1ca8ce43528726184477992c187b121
bonabrian <bonabriansiagian@gmail.com>
2023-06-07 22:21:59 +0700

    feat: add search icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -154,3 +154,22 @@ export const Moon = (props: ComponentProps<'svg'>) => {
     </svg>
   )
 }
+
+export const Search = (props: ComponentProps<'svg'>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  )
+}
