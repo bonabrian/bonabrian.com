9f221c7150c791ffe64c8593f984a261d761e94e
bonabrian <bonabriansiagian@gmail.com>
2023-06-07 23:21:30 +0700

    feat: redesign snippets page

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,8 +1,10 @@
+import cx from 'classnames'
 import type { Snippet } from 'contentlayer/generated'
 import { allSnippets } from 'contentlayer/generated'
 import type { Metadata } from 'next'
 import { Suspense } from 'react'
 
+import Container from '@/components/container'
 import Link from '@/components/link'
 import PageHeader from '@/components/page-header'
 import { routes } from '@/lib/constants'
@@ -29,38 +31,67 @@ export const metadata: Metadata = getMetadata({
 const SnippetsPage = async () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="my-4 space-y-3 md:space-y-5">
-        <PageHeader
-          title="Snippets"
-          description="A repository of previously utilized and saved code snippets which can be copied and pasted."
-        />
-        {snippets.length ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row auto-rows-auto gap-4">
-            {snippets.map(({ slug, title, description, date }) => {
-              const timestamp = formatDate({ timestamp: date, month: 'short' })
-              return (
-                <Link
-                  key={slug}
-                  href={`${routes.SNIPPETS}/${slug}`}
-                  className="flex flex-col justify-between border-2 border-solid border-gray-900 dark:border-slate-100 rounded-2xl p-4 w-full hover:border-primary-500 dark:hover:border-primary-500 transition-all"
-                >
-                  <h3 className="font-bold mb-1 text-base sm:text-lg">
-                    {title}
-                  </h3>
-                  <p className="text-base">{description}</p>
-                  <p
-                    className="mt-4 text-sm text-gray-900/50 dark:text-white/60 text-right"
-                    title={timestamp.raw}
+      <PageHeader
+        title="Snippets"
+        description="A repository of previously utilized and saved code snippets which can be copied and pasted."
+      />
+      <div id="content">
+        <Container>
+          {snippets.length ? (
+            <div
+              className={cx(
+                'grid grid-cols-1 grid-flow-row auto-rows-auto gap-4',
+                'md:grid-cols-2',
+                'lg:grid-cols-3',
+              )}
+            >
+              {snippets.map(({ slug, title, description, date }) => {
+                const timestamp = formatDate({
+                  timestamp: date,
+                })
+
+                return (
+                  <Link
+                    key={slug}
+                    href={`${routes.SNIPPETS}/${slug}`}
+                    className={cx(
+                      'flex flex-col justify-between border border-slate-100 rounded-2xl p-6',
+                      'dark:border-gray-800',
+                    )}
                   >
-                    {timestamp.formatted}
-                  </p>
-                </Link>
-              )
-            })}
-          </div>
-        ) : (
-          <p className="text-center">No snippets.</p>
-        )}
+                    <h2
+                      className={cx(
+                        'font-semibold text-xl text-gray-700 mb-2',
+                        'dark:text-slate-50',
+                      )}
+                    >
+                      {title}
+                    </h2>
+                    <p
+                      className={cx(
+                        'mb-4 text-gray-600',
+                        'dark:text-slate-200',
+                      )}
+                    >
+                      {description}
+                    </p>
+                    <p
+                      className={cx(
+                        'text-xs text-gray-900/60',
+                        'dark:text-slate-100/70',
+                      )}
+                      title={timestamp.raw}
+                    >
+                      {timestamp.formatted}
+                    </p>
+                  </Link>
+                )
+              })}
+            </div>
+          ) : (
+            <p className={cx('text-center')}>No snippets.</p>
+          )}
+        </Container>
       </div>
     </Suspense>
   )
