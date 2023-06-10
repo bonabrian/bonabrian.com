a80e5e905f42044a524052f2e668e9023fec34f7
bonabrian <bonabriansiagian@gmail.com>
2023-06-11 03:34:30 +0700

    feat: add useViews hook

---
+++ src/hooks/useViews.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+
+import { useRequest } from './useRequest'
+
+export const useViews = ({
+  slug,
+  trackView,
+}: {
+  slug: string
+  trackView?: boolean
+}) => {
+  const { data, loading } = useRequest<{ total: number }>(`/api/views/${slug}`)
+
+  const registerView = (slug: string) => {
+    fetch(`/api/views/${slug}`, {
+      method: 'POST',
+    })
+  }
+
+  useEffect(() => {
+    if (trackView) {
+      registerView(slug)
+    }
+  }, [slug, trackView])
+
+  return { views: data, loading }
+}
