907654587d9636932208815722ed047adef6f7f2
bonabrian <bonabriansiagian@gmail.com>
2023-06-13 19:03:42 +0700

    feat: add source props on mdx image

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import classnames from 'classnames'
+import cx from 'classnames'
 import type { ImageProps as NextImageProps } from 'next/image'
 import NextImage from 'next/image'
 import { useState } from 'react'
 
+import { getDomainFromUrl } from '@/lib/utils'
+
 import ImageLightBox from '../image-lightbox'
+import Link from '../link'
 
 type BaseImageProps = Omit<NextImageProps, 'width' | 'height' | 'fill'>
 type SizeProps = BaseImageProps & { size?: number }
@@ -17,10 +20,12 @@ type DimensionProps = BaseImageProps & {
 
 type ImageProps = (SizeProps | DimensionProps) & {
   shouldOpenLightBox?: boolean
+  source?: string
 }
 
 const Image = ({
   shouldOpenLightBox = true,
+  source,
   className,
   ...props
 }: ImageProps) => {
@@ -38,24 +43,36 @@ const Image = ({
   }
 
   return (
-    <figure
-      className={classnames(fill ? 'relative aspect-video' : '', className)}
-    >
-      <NextImage
-        {...rest}
-        width={fill ? undefined : width ?? undefined}
-        height={fill ? undefined : height ?? undefined}
-        loading={props.priority ? undefined : props.loading}
-        decoding="async"
-        className={classnames(
-          'object-cover rounded-xl',
-          shouldOpenLightBox ? 'cursor-zoom-in' : '',
+    <figure>
+      <div className={cx(fill ? 'relative aspect-video' : '', className)}>
+        <NextImage
+          {...rest}
+          width={fill ? undefined : width ?? undefined}
+          height={fill ? undefined : height ?? undefined}
+          loading={props.priority ? undefined : props.loading}
+          decoding="async"
+          className={cx(
+            'object-cover rounded-xl',
+            shouldOpenLightBox ? 'cursor-zoom-in' : '',
+          )}
+          onClick={handleOpenLightBox}
+          fill={fill}
+        />
+        {openLightBox && (
+          <ImageLightBox
+            closeLightBox={() => setOpenLightBox(false)}
+            {...rest}
+          />
         )}
-        onClick={handleOpenLightBox}
-        fill={fill}
-      />
-      {openLightBox && (
-        <ImageLightBox closeLightBox={() => setOpenLightBox(false)} {...rest} />
+      </div>
+      {source && (
+        <figcaption
+          className={cx('text-xs text-gray-900/60 dark:text-slate-100/70 mt-1')}
+        >
+          <span className={cx('flex items-center justify-center gap-1')}>
+            Source <Link href={source ?? ''}>{getDomainFromUrl(source)}</Link>
+          </span>
+        </figcaption>
       )}
     </figure>
   )
