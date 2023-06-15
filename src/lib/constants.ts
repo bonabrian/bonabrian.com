3dcadf47ee2cdf0daf4c7319be164d0b77e31810
bonabrian <bonabriansiagian@gmail.com>
2023-06-16 02:48:10 +0700

    feat: add constants for maximum user reactions

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,3 +7,7 @@ export const routes = {
   STATS: '/stats',
   RESUME: '/resume',
 }
+
+export const MAX_VIEWS_PER_SESSION = 30
+export const MAX_SHARES_PER_SESSION = 10
+export const MAX_REACTIONS_PER_SESSION = 20
