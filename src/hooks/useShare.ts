e00a8deae64bf6828dd60cfbfd78e5a2ce3012e2
bonabrian <bonabriansiagian@gmail.com>
2023-06-17 02:06:24 +0700

    feat: add useShare hooks

---
+++ src/hooks/useShare.ts
@@ -0,0 +1,20 @@
+import type { ShareType } from '@prisma/client'
+
+import { useRequest } from './useRequest'
+
+export const useShare = (slug: string) => {
+  const { data, mutate, loading } = useRequest<{ total: number }>(
+    `/api/shares/${slug}`,
+  )
+
+  const addShare = (type: ShareType) => {
+    // optimistic update
+    mutate(Object.assign({}, data, { total: (data?.total ?? 0) + 1 }), false)
+    fetch(`/api/shares/${slug}`, {
+      method: 'POST',
+      body: JSON.stringify({ type }),
+    })
+  }
+
+  return { shares: data, addShare, loading }
+}
