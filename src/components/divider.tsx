17898471329747608ce22f0fc0f1015780cccaee
bonabrian <bonabriansiagian@gmail.com>
2023-09-16 16:28:46 +0700

    fix: divider color

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -4,7 +4,7 @@ const Divider = () => {
   return (
     <hr
       className={cn(
-        'w-full h-8 my-2 text-2xl border-none text-center font-bold animate-pulse before:content-["∿∿∿"]',
+        'w-full h-8 my-2 text-2xl border-none text-center font-bold animate-pulse before:content-["∿∿∿"] before:text-primary',
       )}
     />
   )
