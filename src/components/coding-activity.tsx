77d2e71dac0a675c192b0ecf7b209be6332fee7c
bonabrian <bonabriansiagian@gmail.com>
2023-09-17 23:47:26 +0700

    feat: optimize when loading data

--- src/components/coding-activity.tsx
+++ src/components/coding-activity.tsx
@@ -8,13 +8,14 @@ import { useWakatime } from '@/hooks'
 import cn from '@/lib/cn'
 import { formatDate } from '@/lib/utils'
 
+import { Spinner } from './common'
 import OverviewCard from './common/overview-card'
 import Progress from './common/progress'
 import { WakaTime as WakaTimeIcon } from './icons'
 import Link from './link'
 
 const CodingActivity = () => {
-  const { data } = useWakatime()
+  const { data, loading } = useWakatime()
 
   const [formattedLastModifiedDate, setFormattedLastModifiedDate] = useState<
     string | null
@@ -40,8 +41,8 @@ const CodingActivity = () => {
     formatLastModified()
   }, [data])
 
-  const startDate = data?.start ? formatDate(data?.start) : ''
-  const endDate = data?.end ? formatDate(data?.end) : ''
+  const startDate = data?.start ? formatDate(data?.start) : 'N/A'
+  const endDate = data?.end ? formatDate(data?.end) : 'N/A'
   const dailyAverage =
     data?.human_readable_daily_average_including_other_language ?? 'N/A'
   const thisWeekTotal =
@@ -70,7 +71,7 @@ const CodingActivity = () => {
   ]
 
   return (
-    <div className={cn('flex flex-col gap-y-2 bg-gradie')}>
+    <div className={cn('flex flex-col gap-y-2')}>
       <div className={cn('flex items-center gap-1 text-xl font-medium')}>
         <WakaTimeIcon />
         <h2 className={cn('capitalize')}>Weekly Coding Activities</h2>
@@ -99,43 +100,58 @@ const CodingActivity = () => {
           Last modified:{' '}
           {formattedLastModifiedDate ? (
             <span>{formattedLastModifiedDate}</span>
-          ) : null}
+          ) : (
+            'N/A'
+          )}
         </div>
       </div>
 
-      <div className={cn('mb-1 grid md:grid-cols-2 gap-3 py-2')}>
-        <OverviewCard label="Start Date" value={startDate} />
-        <OverviewCard label="End Date" value={endDate} />
-        <OverviewCard label="Daily Coding Average" value={dailyAverage} />
-        <OverviewCard label="This Week Coding Time" value={thisWeekTotal} />
-        <OverviewCard label="Best Day Coding Time" value={bestDay} />
-        <OverviewCard label="All Time Since Today" value={allTimeSinceToday} />
-      </div>
-
-      <div className={cn('flex flex-col mt-2 gap-4', 'sm:flex-row')}>
-        {activities.map((activity) => (
-          <div
-            key={activity.title}
-            className={cn(
-              'relative flex flex-1 flex-col gap-2 rounded-md p-0.5',
-              activity.bg,
-            )}
-          >
-            <div className={cn('h-full w-full rounded-md bg-background')}>
-              <p className={cn('absolute -top-3 left-3 px-2 bg-background')}>
-                {activity.title}
-              </p>
-              <ul className={cn('flex flex-col py-3 px-4 gap-1')}>
-                {activity.data.map((item) => (
-                  <li key={item.name}>
-                    <Progress data={item} className={cn(activity.bg)} />
-                  </li>
-                ))}
-              </ul>
-            </div>
+      {loading ? (
+        <div className={cn('flex items-center justify-center')}>
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <div className={cn('mb-1 grid md:grid-cols-2 gap-3 py-2')}>
+            <OverviewCard label="Start Date" value={startDate} />
+            <OverviewCard label="End Date" value={endDate} />
+            <OverviewCard label="Daily Coding Average" value={dailyAverage} />
+            <OverviewCard label="This Week Coding Time" value={thisWeekTotal} />
+            <OverviewCard label="Best Day Coding Time" value={bestDay} />
+            <OverviewCard
+              label="All Time Since Today"
+              value={allTimeSinceToday}
+            />
           </div>
-        ))}
-      </div>
+
+          <div className={cn('flex flex-col mt-2 gap-4', 'sm:flex-row')}>
+            {activities.map((activity) => (
+              <div
+                key={activity.title}
+                className={cn(
+                  'relative flex flex-1 flex-col gap-2 rounded-md p-0.5',
+                  activity.bg,
+                )}
+              >
+                <div className={cn('h-full w-full rounded-md bg-background')}>
+                  <p
+                    className={cn('absolute -top-3 left-3 px-2 bg-background')}
+                  >
+                    {activity.title}
+                  </p>
+                  <ul className={cn('flex flex-col py-3 px-4 gap-1')}>
+                    {activity.data.map((item) => (
+                      <li key={item.name}>
+                        <Progress data={item} className={cn(activity.bg)} />
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   )
 }
