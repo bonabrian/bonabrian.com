039013eb337e28f57f33b9f5dc20eecaff49dc98
bonabrian <bonabrian.dev@gmail.com>
2023-09-20 19:34:37 +0700

    feat: theme switch enhancement

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,50 +1,206 @@
 'use client'
 
-import { Switch } from '@headlessui/react'
+import { m, useMotionValue, useTransform } from 'framer-motion'
 
 import { useTheme } from '@/hooks'
 import cn from '@/lib/cn'
 
-import { Moon, Sun } from './icons'
+import { Button } from './common'
 
 const ThemeSwitch = () => {
   const { theme, mounted, setTheme } = useTheme()
   const isDark = theme === 'dark'
 
+  const duration = 0.7
+
+  const moonVariants = {
+    checked: {
+      scale: 1,
+    },
+    unchecked: {
+      scale: 0,
+    },
+  }
+
+  const sunVariants = {
+    checked: {
+      scale: 0,
+    },
+    unchecked: {
+      scale: 1,
+    },
+  }
+
+  const scaleMoon = useMotionValue(isDark ? 1 : 0)
+  const scaleSun = useMotionValue(isDark ? 0 : 1)
+  const pathLengthMoon = useTransform(scaleMoon, [0.6, 1], [0, 1])
+  const pathLengthSun = useTransform(scaleSun, [0.6, 1], [0, 1])
+
   return (
-    <div className={cn('flex items-center w-12 h-6 ml-4')}>
-      {mounted && (
-        <Switch
-          checked={isDark}
-          onChange={() => setTheme(isDark ? 'light' : 'dark')}
-          className={cn(
-            'relative inline-flex shrink-0 h-6 w-12 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 bg-slate-200',
-            'dark:bg-gray-600',
-          )}
+    <Button
+      variant="ghost"
+      onClick={() => setTheme(isDark ? 'light' : 'dark')}
+      className={cn('px-2 py-1 z-50')}
+    >
+      <m.div
+        initial={true}
+        animate={isDark ? 'checked' : 'unchecked'}
+        transition={{ duration }}
+      >
+        <m.svg
+          width="20"
+          height="20"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <span className="sr-only">Use settings</span>
-          <div
-            className={cn(
-              'flex items-center border-4 h-6 w-6 border-transparent absolute text-gray-900 transition ease-in-out duration-200',
-              'dark:text-gray-100',
-            )}
-          >
-            {isDark ? (
-              <Sun className={cn('translate-x-6')} />
-            ) : (
-              <Moon className={cn('translate-x-0')} />
-            )}
-          </div>
-          <span
-            aria-hidden="true"
-            className={cn(
-              'bg-gray-100 pointer-events-none inline-block h-6 w-6 rounded-full shadow-lg transform ring-0 transition ease-in-out duration-200 translate-x-6',
-              'dark:translate-x-0',
-            )}
-          />
-        </Switch>
-      )}
-    </div>
+          {mounted && (
+            <>
+              <m.path
+                d="M12.4058 17.7625C15.1672 17.7625 17.4058 15.5239 17.4058 12.7625C17.4058 10.0011 15.1672 7.76251 12.4058 7.76251C9.64434 7.76251 7.40576 10.0011 7.40576 12.7625C7.40576 15.5239 9.64434 17.7625 12.4058 17.7625Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M12.4058 1.76251V3.76251"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M12.4058 21.7625V23.7625"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M4.62598 4.98248L6.04598 6.40248"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M18.7656 19.1225L20.1856 20.5425"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M1.40576 12.7625H3.40576"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M21.4058 12.7625H23.4058"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M4.62598 20.5425L6.04598 19.1225"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M18.7656 6.40248L20.1856 4.98248"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                variants={sunVariants}
+                custom={isDark}
+                transition={{ duration }}
+                style={{
+                  pathLength: pathLengthSun,
+                  scale: scaleSun,
+                }}
+              />
+              <m.path
+                d="M21.1918 13.2013C21.0345 14.9035 20.3957 16.5257 19.35 17.8781C18.3044 19.2305 16.8953 20.2571 15.2875 20.8379C13.6797 21.4186 11.9398 21.5294 10.2713 21.1574C8.60281 20.7854 7.07479 19.9459 5.86602 18.7371C4.65725 17.5283 3.81774 16.0003 3.4457 14.3318C3.07367 12.6633 3.18451 10.9234 3.76526 9.31561C4.346 7.70783 5.37263 6.29868 6.72501 5.25307C8.07739 4.20746 9.69959 3.56862 11.4018 3.41132C10.4052 4.75958 9.92564 6.42077 10.0503 8.09273C10.175 9.76469 10.8957 11.3364 12.0812 12.5219C13.2667 13.7075 14.8384 14.4281 16.5104 14.5528C18.1823 14.6775 19.8435 14.1979 21.1918 13.2013Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transition={{ duration }}
+                variants={moonVariants}
+                custom={isDark}
+                style={{
+                  pathLength: pathLengthMoon,
+                  scale: scaleMoon,
+                }}
+              />
+            </>
+          )}
+        </m.svg>
+      </m.div>
+    </Button>
   )
 }
 
