bb0ee30097574e2d5bf33b95263bedd042995796
Bona Brian Siagian <bonabrian.dev@gmail.com>
2024-09-07 12:51:38 +0700

    refactor(about): remove unused component

--- components/about/index.ts
+++ components/about/index.ts
@@ -1,6 +1,5 @@
 import Biography from './biography';
 import OpenForHire from './open-for-hire';
-import Summary from './summary';
 import TechStacks from './tech-stacks';
 
-export { Biography, OpenForHire, Summary, TechStacks };
+export { Biography, OpenForHire, TechStacks };
--- components/about/summary.tsx
+++
@@ -1,3 +0,0 @@
-const Summary = () => {};
-
-export default Summary;
