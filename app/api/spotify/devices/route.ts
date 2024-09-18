829813cd62b97fcfcd534e971b09ebb1f045c371
Bona Brian Siagian <bonabrian.dev@gmail.com>
2024-09-18 22:27:18 +0700

    feat(now-playing): force no store for devices api

--- app/api/spotify/devices/route.ts
+++ app/api/spotify/devices/route.ts
@@ -3,6 +3,7 @@ import { response } from '@/lib/server';
 import type { APIErrorResponse, APIListResponse } from '@/types/server';
 import type { Device } from '@/types/spotify';
 
+export const fetchCache = 'force-no-store';
 export const dynamic = 'force-dynamic';
 
 export const GET = async () => {
