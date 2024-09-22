27dddabd6b0f2430f7551ae28bd3fcc634fdefb3
Bona Brian Siagian <bonabrian.dev@gmail.com>
2024-09-22 13:16:09 +0700

    feat(og): add og image generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next';
 
 import { Hero } from '@/components/home';
+import { SITE } from '@/constants';
 import { seo } from '@/lib/meta';
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = seo({
+  title: SITE.title,
   url: '/',
 });
 
